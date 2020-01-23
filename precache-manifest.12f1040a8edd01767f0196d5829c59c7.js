7fa63b5c980ced0fcf1bde8f5262d3cdd3c28457
VirtualCucumber <45335671+VirtualCucumber@users.noreply.github.com>
2020-01-23 12:11:26 -0600

    Updates

---
+++ precache-manifest.12f1040a8edd01767f0196d5829c59c7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3e24e2522080dba622a2830a6f687e35",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3686a39dcd866d64ba12",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "a96dc52544b11dec6373",
+    "url": "/static/css/main.8c123657.chunk.css"
+  },
+  {
+    "revision": "3686a39dcd866d64ba12",
+    "url": "/static/js/2.f2a55b25.chunk.js"
+  },
+  {
+    "revision": "fb449a4c5a46c8b48e44463d73fd4077",
+    "url": "/static/js/2.f2a55b25.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a96dc52544b11dec6373",
+    "url": "/static/js/main.5bb01f6d.chunk.js"
+  },
+  {
+    "revision": "9d5958f479087e595537",
+    "url": "/static/js/runtime-main.e9159130.js"
+  }
+]);
